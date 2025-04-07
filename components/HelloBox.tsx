6d313bf8dbbe4c6bea6d9fc29c83e51f30c7be1e
ddoonglog <ljb100479@naver.com>
2025-04-08 00:44:58 +0900

    Create HelloBox.tsx

---
+++ components/HelloBox.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function HelloBox() {
+  return (
+    <div style={{ marginTop: '20px', padding: '16px', background: '#eee' }}>
+      👋 안녕하세요! 여기는 둥둥로그 툴의 테스트 박스입니다.
+    </div>
+  )
+}
